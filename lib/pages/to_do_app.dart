11e1959f38c43df9a8ab1877a962cd13192c7146
Bougainville Chai <kajallibi@gmail.com>
2024-07-22 01:14:32 +0530

    Initial Commit

---
+++ lib/pages/to_do_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class toDoApp extends StatefulWidget {
+  const toDoApp({Key? key}) : super(key: key);
+
+  @override
+  State<toDoApp> createState() => _toDoAppState();
+}
+
+class _toDoAppState extends State<toDoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+C:\Users\Kajal Libi\AndroidStudioProjects\DartProj\todoapp
